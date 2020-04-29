11553b38a91ac11b570da05ff4b740e8aebe9dd2
basaranb <TCBBADUR@turkcell.entp.tgc>
2020-04-29 17:28:43 +0300

    Updates

---
+++ precache-manifest.d82cc77ad76e5fb22015386905fb8ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954ad3d215e0dde6db9eebcb004289c2",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "0e607b45e39da32c8a54",
+    "url": "/monster-cards/static/css/main.1e5717e2.chunk.css"
+  },
+  {
+    "revision": "b8f5f0db7fe387c3cede",
+    "url": "/monster-cards/static/js/2.16262685.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cards/static/js/2.16262685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e607b45e39da32c8a54",
+    "url": "/monster-cards/static/js/main.5fbeda96.chunk.js"
+  },
+  {
+    "revision": "3da9d671a1c73a0148d3",
+    "url": "/monster-cards/static/js/runtime-main.c735afd0.js"
+  }
+]);
